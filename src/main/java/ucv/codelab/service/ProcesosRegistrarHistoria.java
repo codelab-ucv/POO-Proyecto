a8a47e7f73f86d9d10f26919a5d499b0c4a6c5c2
Adrian9904 <adriandc9904@gmail.com>
2025-07-09 09:47:05 -0500

    Guarda historia completa en BDD

--- src/main/java/ucv/codelab/service/ProcesosRegistrarHistoria.java
+++ src/main/java/ucv/codelab/service/ProcesosRegistrarHistoria.java
@@ -3,13 +3,20 @@ package ucv.codelab.service;
 import java.sql.Connection;
 import java.util.Optional;
 
+import ucv.codelab.enumerados.TipoDiagnostico;
+import ucv.codelab.model.Diagnostico;
+import ucv.codelab.model.ExamenFisico;
 import ucv.codelab.model.HistoriaClinica;
 import ucv.codelab.model.Medico;
 import ucv.codelab.model.Paciente;
+import ucv.codelab.model.Tratamiento;
+import ucv.codelab.repository.DiagnosticoRepository;
+import ucv.codelab.repository.ExamenFisicoRepository;
 import ucv.codelab.repository.HistoriaClinicaRepository;
 import ucv.codelab.repository.MedicoRepository;
 import ucv.codelab.repository.MySQLConexion;
 import ucv.codelab.repository.PacienteRepository;
+import ucv.codelab.repository.TratamientoRepository;
 import ucv.codelab.util.ComprobarDatos;
 import ucv.codelab.util.Mensajes;
 import ucv.codelab.view.FrmRegistroHistoriaC;
@@ -61,16 +68,97 @@ public class ProcesosRegistrarHistoria {
         historiaClinica.setTiempoEnfermedad(tiempoEnfermedad);
         historiaClinica.setObservaciones(observaciones);
 
-        // TODO pendiente cargar los datos de examen fisico, diagnostico y tratamiento
-
         try (Connection conn = new MySQLConexion().getConexion()) {
             HistoriaClinicaRepository repo = new HistoriaClinicaRepository(conn);
             repo.crear(historiaClinica);
+
+            ExamenFisico examenFisico = loadExamenFisico(view);
+            if (examenFisico != null) {
+                // Establece el ID de la historia en el obtenido al insertar
+                examenFisico.setIdHistoria(historiaClinica.getIdHistoria());
+                ExamenFisicoRepository examenFisicoRepository = new ExamenFisicoRepository(conn);
+                examenFisicoRepository.crear(examenFisico);
+            }
+
+            Diagnostico diagnostico = loadDiagnostico(view);
+            if (diagnostico != null) {
+                // Establece el ID de la historia en el obtenido al insertar
+                diagnostico.setIdHistoria(historiaClinica.getIdHistoria());
+                DiagnosticoRepository diagnosticoRepository = new DiagnosticoRepository(conn);
+                diagnosticoRepository.crear(diagnostico);
+            }
+
+            Tratamiento tratamiento = loadTratamiento(view);
+            if (tratamiento != null) {
+                // Establece el ID de la historia en el obtenido al insertar
+                tratamiento.setIdHistoria(historiaClinica.getIdHistoria());
+                TratamientoRepository tratamientoRepository = new TratamientoRepository(conn);
+                tratamientoRepository.crear(tratamiento);
+            }
+
             return true;
         } catch (Exception e) {
             Mensajes.error("Error al guardar", "Verifique los datos ingresados");
             return false;
         }
+    }
+
+    private static ExamenFisico loadExamenFisico(FrmRegistroHistoriaC view) {
+        Double peso = ComprobarDatos.validarDecimal(view.txtPesoPaciente.getText());
+        Double talla = ComprobarDatos.validarDecimal(view.txtTallaPaciente.getText());
+        String presionArterial = ComprobarDatos.limpiarString(view.txtPresionArterial.getText());
+        Double temperatura = ComprobarDatos.validarDecimal(view.txtTemperaturaCorporal.getText());
+        Integer frecuenciaCardiaca = ComprobarDatos.validarEntero(view.txtFrecuenciaCardiaca.getText());
+        Integer frecuenciaRespiratoria = ComprobarDatos.validarEntero(view.txtFrecuenciaRespiratoria.getText());
+
+        // Si por lo menos uno de los datos existe retorna el objeto
+        if (peso != null || talla != null || presionArterial != null || temperatura != null
+                || frecuenciaCardiaca != null || frecuenciaRespiratoria != null) {
+            ExamenFisico examenFisico = new ExamenFisico();
+            examenFisico.setPeso(peso);
+            examenFisico.setTalla(talla);
+            examenFisico.setPresionArterial(presionArterial);
+            examenFisico.setTemperatura(temperatura);
+            examenFisico.setFrecuenciaCardiaca(frecuenciaCardiaca);
+            examenFisico.setFrecuenciaRespiratoria(frecuenciaRespiratoria);
+            return examenFisico;
+        }
+        return null;
+    }
+
+    private static Diagnostico loadDiagnostico(FrmRegistroHistoriaC view) {
+        // Verifica primero los ComboBox
+        if (view.cmbTipoDiagnostico.getSelectedItem() == null) {
+            return null;
+        }
+
+        String tipoDiagnostico = ComprobarDatos.limpiarString(view.cmbTipoDiagnostico.getSelectedItem().toString());
+        String descripcion = ComprobarDatos.limpiarString(view.txtAreaDescripcionDiagnostico.getText());
+        String codigoCie = ComprobarDatos.limpiarString(view.txtCodigoCie10.getText());
+
+        // Si el tipo de diagnostico no es nulo ni el codigo CIE10 retorna el objeto
+        if (tipoDiagnostico != null && codigoCie != null) {
+            Diagnostico diagnostico = new Diagnostico();
+            diagnostico.setTipo(TipoDiagnostico.fromString(tipoDiagnostico));
+            diagnostico.setCodigoCIE10(codigoCie);
+            diagnostico.setDescripcion(descripcion);
+            return diagnostico;
+        }
+        return null;
+    }
+
+    private static Tratamiento loadTratamiento(FrmRegistroHistoriaC view) {
+        String descripcion = ComprobarDatos.limpiarString(view.txtAreaDescripcionTratamiento.getText());
+        String indicaciones = ComprobarDatos.limpiarString(view.txtAreaIndicaciones.getText());
+
+        // Si por lo menos uno de los datos existe retorna el objeto
+        if (descripcion != null || indicaciones != null) {
+            Tratamiento tratamiento = new Tratamiento();
+            tratamiento.setDescripcion(descripcion);
+            tratamiento.setIndicaciones(indicaciones);
+            return tratamiento;
+        }
+        return null;
     }
 
     public static void limpiarEntradas(FrmRegistroHistoriaC view) {
